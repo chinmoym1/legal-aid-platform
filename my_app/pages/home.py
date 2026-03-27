8688923523bacd1ecf17cd89fa0f4da52ed20f49
chinmoy <124980162+chinmoym1@users.noreply.github.com>
2026-03-28 00:54:31 +0530

    Comment out switch_page import in home.py
    Comment out the import statement for switch_page.

--- my_app/pages/home.py
+++ my_app/pages/home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
+# from streamlit_extras.switch_page_button import switch_page
 
 def show_home():
     # --- PAGE CONFIG ---
